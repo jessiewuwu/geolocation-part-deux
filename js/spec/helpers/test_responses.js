5aed48bb57a6a1e209c8b278f49486accce9f041
jessiewuwu <jwu004@gmail.com>
2015-05-11 11:41:06 -0700

    pseudocode for AJAX jasmine tests

---
+++ js/spec/helpers/test_responses.js
@@ -0,0 +1,10 @@
+
+var TestResponses = {
+  search: {
+    success: {
+      status: 200,
+      responseText: {"ip":"216.58.219.206","country_code":"US","country_name":"United States","region_code":"CA","region_name":"California","city":"Mountain View","zip_code":"94043","time_zone":"America/Los_Angeles","latitude":37.419,"longitude":-122.058,"metro_code":807}
+
+    }
+  }
+}
