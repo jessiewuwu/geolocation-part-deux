b0e6e63ca7fdc48a70a6df8fabc1d09c985cb4ce
jessiewuwu <jwu004@gmail.com>
2015-05-10 20:15:42 -0700

    On click, map works for My Location only or Company Location only

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,3 +1,24 @@
+// google maps
+function initialize(latitude, longitude) {
+	$('#map-canvas').empty();
+	var myLatLng = new google.maps.LatLng(latitude,longitude);
+  var mapCanvas = document.getElementById('map-canvas');
+  var mapOptions = {
+    center: myLatLng,
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions)
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		// icon: 'http://oi62.tinypic.com/2mhu3rr.jpg'
+	});
+	google.maps.event.addDomListener(window, 'load', initialize);
+}
+
+
+
 function updateLocationDetails(data){
 	var now = new Date();
 
@@ -8,6 +29,8 @@ function updateLocationDetails(data){
 	$("#location_timezone").html(data.timezone);
 	$("#location_lat").html(data.lat);
 	$("#location_lon").html(data.lon);
+
+	initialize(data.lat, data.lon);
 
 	$("table").removeClass("empty");
 	$(".help").click(function(e){
@@ -59,43 +82,77 @@ function initializePage(){
 	}));
 }
 
+// backbone model
+var Company = Backbone.Model.extend({
+	initialize: function(){
+		// console.log('backbone model is working');
+	},
+	defaults: {
+		ip: '',
+		country_code: '',
+		country_name: '',
+		region_code: '',
+		region_name: '',
+		city: '',
+		zip_code: '',
+		time_zone: '',
+		latitude: '',
+		longitude: '',
+		metro_code: ''
+	},
+	urlRoot: '/'
+});
+var searchCompany = new Company();
 
 
-
-
-
-function getCompanyLocation(){
-	$('#search-button').on('click', function(e){
-		e.preventDefault();
-		console.log('search prevent default works');
-		var formData = $('#search-text-area').val();
-		console.log(formData);
-		$.ajax({
-			url: 'https://freegeoip.net/json/' + formData,
-			type: 'GET',
-			dataType: 'json'
+// backbone
+	//view
+var CompanyView = Backbone.View.extend({
+	tagName: "div",
+	className: "company",
+	render: function(){
+		console.log('this is the view')
+		// return this;
+	},
+	// events: {
+	// 	'click #search-button' : 'getCompanyMap',
+	// 	'click img': 'testing'
+	// },
+	getCompanyMap: function(e){
+		$('#search-button').on('click', function(e){
+			e.preventDefault();
+			var formData = $('#search-text-area').val();
+			Backbone.ajax({
+				url: 'https://freegeoip.net/json/' + formData,
+				type: 'GET',
+				dataType: 'json'
+			})
+			.done(function(data) {
+				searchCompany.set('ip', data.ip);
+				searchCompany.set('city', data.city);
+				searchCompany.set('zip_code', data.zip_code);
+				searchCompany.set('longitude', data.longitude);
+				searchCompany.set('latitude', data.latitude);
+				initialize(data.latitude, data.longitude);
+			})
+			.fail(function() {
+				console.log("error");
+			});
 		})
-		.done(function(data) {
-			console.log("success");
-			renderMap(data);
-		})
-		.fail(function() {
-			console.log("error");
-		})
-		.always(function() {
-			console.log("complete");
-		});
+	}
 
-	})
-}
+})
 
-function renderMap(data){
-	console.log('render map inside of the ajax function works')
-	$('.website-search').append('<h1>IT WORKS! IT APPENDS!</h1>');
-	console.log(data.latitude);
-}
+var companyView = new CompanyView({
+	model: searchCompany
+});
+
+
+
+
 
 $(document).ready(function(){
 	initializePage();
-	getCompanyLocation();
+	companyView.render();
+	companyView.getCompanyMap();
 });
--- pseudocode.md
+++ pseudocode.md
@@ -1,14 +1,21 @@
 ## Schedule
 Saturday - functionality
 - set up Bootstrap
-- add functioning search form with API working
-- implement Google Maps API + Snazzy Map
-- use Backbone for the Model, View
+- add functioning search form with GeoIP API working
+- implement Google Maps API
+- review the original code
+* use Backbone and Mustache for the Model, View
+* validations with reg ex for website domains
 
 Sunday - design + meeting requirements
 - finish whatever wasn't finished yesterday
+- add My Location on click
+  - right now it takes two clicks to get the data
+  - right now it replaces the map, doesn't add My Location to map
 - come up with a wireframe
+- Implement Snazzy Map
 - Responsive Design with media queries
+- use SASS
 - Testing: 80%+ on the features
 - create the README.md
 
